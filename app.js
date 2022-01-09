ee5ab0924db88a85b0796c35881c9d13851b0062
Dannycodes305 <89235491+Dannycodes305@users.noreply.github.com>
2022-01-09 13:05:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//menu animation//
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
